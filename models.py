f0018145bad662e8a86a7cbe46660eadad297467
zoezhang926 <zoezhang926@gmail.com>
2020-08-29 23:09:58 -0400

    add models, forms and templates

---
+++ models.py
@@ -0,0 +1,80 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+def connect_db(app):
+    """ Connect to database"""
+    
+    db.app = app
+    db.init_app(app)
+    
+class User(db.Model):
+    """Create User Model"""
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), 
+                         nullable=False, 
+                         unique=True,
+                         primary_key=True)
+
+    password = db.Column(db.Text, 
+                         nullable=False)
+    
+    email = db.Column(db.String(50),
+                      nullable=False,
+                      unique=True)
+
+    first_name = db.Column(db.String(30),
+                           nullable=False)
+    
+    last_name = db.Column(db.String(30),
+                          nullable=False)
+    
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+    
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user, hashing their password."""
+
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
+        
+class Feedback(db.Model):
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
